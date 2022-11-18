0055c5605dfbb76f5466241969c250b4043a1598
Ghanashyam-Bhat <ghanashyambhat6@gmail.com>
2022-11-18 09:41:45 +0530

    sql-connector demo

--- website/mysqlconnector.py
+++ website/mysqlconnector.py
@@ -1,14 +1,15 @@
-import mysql.connector 
+#https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html 
 import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
   user="gb",
-  password="password"
+  password="password",
+  database = "FIT_MNG"
 )
 
-
 mycursor = mydb.cursor()
-mycursor.execute("show databases")
-for x in mycursor:
-    print(x)
+mycursor.execute("select id from user_info")
+for id,fname in mycursor:
+    print(id,fname)
+
